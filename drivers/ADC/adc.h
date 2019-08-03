3a629721eac4a2176956189de1e0c9159df9bf87
plainji <plain_ji@163.com>
2019-08-03 16:34:41 +0800

    format code

--- drivers/ADC/adc.h
+++ drivers/ADC/adc.h
@@ -1,6 +1,8 @@
 #ifndef __ADC_H
 #define __ADC_H	
 
+#include "stm32f10x.h"
+
 u16 Get_Adc(u8 ch);
 void Adc_Init(void);
 void Get_battery_volt(void); 
--- drivers/IIC/IOI2C.h
+++ drivers/IIC/IOI2C.h
@@ -1,41 +1,6 @@
 #ifndef __IOI2C_H
 #define __IOI2C_H
 #include "stm32f10x.h"
-/**************************************************************************
-作者：平衡小车之家
-淘宝店铺：http://shop114407458.taobao.com/
-**************************************************************************/
-// IO口操作宏定义
-#define BITBAND(addr, bitnum) \
-  ((addr & 0xF0000000) + 0x2000000 + ((addr & 0xFFFFF) << 5) + (bitnum << 2))
-#define MEM_ADDR(addr) *((volatile unsigned long *)(addr))
-#define BIT_ADDR(addr, bitnum) MEM_ADDR(BITBAND(addr, bitnum))
-
-// IO口地址映射
-#define GPIOA_ODR_Addr (GPIOA_BASE + 12)  // 0x4001080C
-#define GPIOB_ODR_Addr (GPIOB_BASE + 12)  // 0x40010C0C
-#define GPIOC_ODR_Addr (GPIOC_BASE + 12)  // 0x4001100C
-#define GPIOD_ODR_Addr (GPIOD_BASE + 12)  // 0x4001140C
-#define GPIOE_ODR_Addr (GPIOE_BASE + 12)  // 0x4001180C
-#define GPIOF_ODR_Addr (GPIOF_BASE + 12)  // 0x40011A0C
-#define GPIOG_ODR_Addr (GPIOG_BASE + 12)  // 0x40011E0C
-
-#define GPIOA_IDR_Addr (GPIOA_BASE + 8)  // 0x40010808
-#define GPIOB_IDR_Addr (GPIOB_BASE + 8)  // 0x40010C08
-#define GPIOC_IDR_Addr (GPIOC_BASE + 8)  // 0x40011008
-#define GPIOD_IDR_Addr (GPIOD_BASE + 8)  // 0x40011408
-#define GPIOE_IDR_Addr (GPIOE_BASE + 8)  // 0x40011808
-#define GPIOF_IDR_Addr (GPIOF_BASE + 8)  // 0x40011A08
-#define GPIOG_IDR_Addr (GPIOG_BASE + 8)  // 0x40011E08
-
-#define PCout(n) BIT_ADDR(GPIOC_ODR_Addr, n)  //输出
-#define PCin(n) BIT_ADDR(GPIOC_IDR_Addr, n)   //输入
-
-#define PBout(n) BIT_ADDR(GPIOB_ODR_Addr, n)  //输出
-#define PBin(n) BIT_ADDR(GPIOB_IDR_Addr, n)   //输入
-
-#define PEout(n) BIT_ADDR(GPIOE_ODR_Addr, n)  //输出
-#define PEin(n) BIT_ADDR(GPIOE_IDR_Addr, n)   //输入
 
 // IO方向设置
 #define SDA_IN()              \
@@ -80,5 +45,3 @@ int i2cWrite(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *data);
 int i2cRead(uint8_t addr, uint8_t reg, uint8_t len, uint8_t *buf);
 
 #endif
-
-//------------------End of File----------------------------
